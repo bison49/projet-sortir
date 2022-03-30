45fc2bbfc6ee2d35a5835138e6442e33b4e6c864
valuenocjh <antoine.corvol@gmail.com>
2022-03-30 13:12:31 +0200

    Form Modif Profil

--- src/Form/ModifParticipantType.php
+++ src/Form/ModifParticipantType.php
@@ -6,7 +6,10 @@ use App\Entity\Participant;
 use App\Entity\Site;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -15,16 +18,10 @@ class ModifParticipantType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('pseudo')
-            ->add('nom')
-            ->add('prenom')
-            ->add('telephone')
-            ->add('mail')
+            ->add('pseudo',TextType::class)
+            ->add('telephone', IntegerType::class)
+            ->add('mail', EmailType::class)
             ->add('password', PasswordType::class)
-            ->add('noSite',EntityType::class,["label"=>'Site',
-                'class'=>Site::class,
-                'choice_label'=>'nom'
-            ])
 
 
         ;
