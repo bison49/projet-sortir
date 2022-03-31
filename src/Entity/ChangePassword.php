2283b304a10cd4381b56a13788a1b8d2a705d3d5
valuenocjh <antoine.corvol@gmail.com>
2022-03-31 13:34:43 +0200

    Modif MDP

---
+++ src/Entity/ChangePassword.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Entity;
+use Symfony\Component\Security\Core\Validator\Constraints as SecurityAssert;
+use App\Repository\ChangePasswordRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ChangePasswordRepository::class)
+ */
+class ChangePassword
+{
+    /**
+     * @SecurityAssert\UserPassword(
+     *     message = "Mauvais mot de passe"
+     * )
+     */
+    protected $oldPassword;
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    protected $newPassword;
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    protected $newConfirm;
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getOldPassword(): ?string
+    {
+        return $this->oldPassword;
+    }
+
+    public function setOldPassword(string $oldPassword): self
+    {
+        $this->oldPassword = $oldPassword;
+
+        return $this;
+    }
+
+    public function getNewPassword(): ?string
+    {
+        return $this->newPassword;
+    }
+
+    public function setNewPassword(string $newPassword): self
+    {
+        $this->newPassword = $newPassword;
+
+        return $this;
+    }
+
+    public function getNewConfirm(): ?string
+    {
+        return $this->newConfirm;
+    }
+
+    public function setNewConfirm(string $newConfirm): self
+    {
+        $this->newConfirm = $newConfirm;
+
+        return $this;
+    }
+}
---
+++ src/Form/ChangerMdpType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\ChangePassword;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ChangerMdpType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('oldPassword', PasswordType::class)
+            ->add('newPassword', PasswordType::class)
+            ->add('newConfirm', PasswordType::class)
+            ->add('Valider', SubmitType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => ChangePassword::class,
+        ]);
+    }
+}
